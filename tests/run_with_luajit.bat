fdde7c764cb7ce80cae452f9b788f626fb57f90a
Raymond W. Ko <raymond.w.ko@gmail.com>
2014-10-20 20:49:09 -0400

    rewrite so tests work in new directory

--- tests/run_with_luajit.bat
+++ tests/run_with_luajit.bat
@@ -1,7 +1,12 @@
 @echo off
+pushd %~dp0\..
 cls
-REM ..\luajit-2.0\src\luajit.exe -jv tests.lua > output.txt
+set LUA=..\luas\luajit\luajit.exe
 
-REM ..\luajit-2.0\src\luajit.exe -joff -jv -jp=a correctness_tests.lua > output.txt
-..\luajit-2.0\src\luajit.exe -jp=a correctness_tests.lua > output.txt
-REM ..\luajit-2.0\src\luajit.exe -jv correctness_tests.lua > output.txt
+%LUA% -jv tests\basic_tests.lua > tests\output.txt
+
+%LUA% -jv tests\correctness_tests.lua >> tests\output.txt 2>&1
+REM %LUA% -joff -jv -jp=a correctness_tests.lua >> tests\output.txt 2>&1
+REM %LUA% -jp=a correctness_tests.lua >> tests\output.txt 2>&1
+
+popd
